56a58baf715fb82f5862f3b77c36ac0f13382dc2
Davide Francavilla <davfrancavilla@gmail.com>
2020-10-20 18:36:34 +0200

    Aggiunto codice per mostrare errori della validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+{{-- @if ($errors->any())
+    @dd($errors)
+@endif --}}
+
+
 @section('content')
 <form action="{{route('posts.store')}}" method="post">
         @csrf
@@ -10,9 +15,15 @@
             @endforeach
         </select>
         <label for="title">Titolo</label>
-    <input id="title" type="text" name="title" placeholder="Nome" value="{{old('title')}}">
+        <input id="title" type="text" name="title" placeholder="Nome" value="{{old('title')}}">
+        @foreach ($errors->get('title') as $message)
+            {{$message}}
+        @endforeach
         <label for="body">Testo</label>
         <textarea id="body" name="body" rows="8" cols="80">{{old('body')}}</textarea>
+        @foreach ($errors->get('body') as $message)
+            {{$message}}
+        @endforeach
         <button type="submit">Invia</button>
     </form>
 @endsection
